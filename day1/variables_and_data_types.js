ca7b9b9dbc174178ce00aaf1ca3708debf886d72
Umar Farooq <umarfarooq567800@gmail.com>
2024-07-17 09:20:28 +0500

    activity 1 completed

---
+++ day1/variables_and_data_types.js
@@ -0,0 +1,13 @@
+// task 1 : Declare a variable using `var` , assign it a number, and log its value to console
+
+var myNumber = 99
+
+console.log(myNumber) // logs: 99
+
+// task 2 : Declare a variable using `let` , assign it a number, and log its value to console
+
+let book = "The Conspiracy Club"
+
+console.log(book) // logs: The Conspiracy Club
+
+// Task 3 : 
