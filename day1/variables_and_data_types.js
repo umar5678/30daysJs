a08c2f5dbbfbdc41208f150e94fdce68efe654e3
Umar Farooq <umarfarooq567800@gmail.com>
2024-07-17 09:22:24 +0500

    update

--- day1/variables_and_data_types.js
+++ day1/variables_and_data_types.js
@@ -4,7 +4,7 @@ var myNumber = 99
 
 console.log(myNumber) // logs: 99
 
-// task 2 : Declare a variable using `let` , assign it a number, and log its value to console
+// task 2 : Declare a variable using `let` , assign it a string, and log its value to console
 
 let book = "The Conspiracy Club"
 
